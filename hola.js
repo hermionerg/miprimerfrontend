0698a226e59d4fa15e0ed3c5db7789329f61cb5a
hermionerg <heerg08@gmail.com>
2022-04-06 21:16:21 +0200

    elementos de mi primera web

---
+++ hola.js
@@ -0,0 +1,6 @@
+function holaholita() {
+    // alert("hola herminica");
+    console.log("he clicado");
+    const primi = document.querySelector("#primi");
+    primi.innerHTML = "texto cambiado con js";
+}
